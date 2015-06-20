bb7b54cf5a24903c32bc63a1a9b4a1bb2bf81cc7
a123zyx98 <a123zyx98@gmail.com>
2015-06-20 21:15:43 +0800

    scheduler add date and update decision maker

--- RPi/Java/src/DecisionMaker.java
+++ RPi/Java/src/DecisionMaker.java
@@ -1,3 +1,21 @@
 public class DecisionMaker{
-	State state;
+	DeviceInfo device
+	ArrayList<MotionEvent> eventlist;
+	DecisionMaker(){
+	
+	}
+	boolean Notify(String arduinoid){
+		MotionEvent event = new MotionEvent(new Date(),arduinoid);
+		
+	}
+	
+}
+
+class MotionEvent{
+	Date date;
+	String arduinoid;
+	MotionEvent(Date date,String id){
+		this.date = date;
+		this.arduinoid = id;
+	}
 }
--- RPi/Java/src/Scheduler.java
+++ RPi/Java/src/Scheduler.java
@@ -1,20 +1,31 @@
 import java.util.Date;
 import java.util.ArrayList; 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 public class Scheduler{
 	
 	//http://www.ewdna.com/2011/12/java-timer.html
 	private ArrayList<Job> joblist;
 	//private RF rf;
-	
+	Socket socket;
 	public Scheduler(){
 		joblist = new ArrayList<>();
 	}
 	public void initial(){
 		
 	}
-	boolean addJob(int targetID,boolean targetcmd,Date targetdate){
+	boolean addJob(String targetID,boolean targetcmd,Date targetdate){
 		Job j = new(targetdate,targetID,targetcmd)
 		return addJob(j);
+	}
+	boolean addJob(String targetID,boolean targetcmd,String datesting){
+		try{
+			Date date = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(datesting);
+		}catch(ParseException e){
+			System.out.println("ParseException");
+			e.printStackTrace();
+		}
+		return addJob(targetID,targetcmd,date);
 	}
 	boolean addJob(Job j){
 		int i;
@@ -41,15 +52,17 @@ public class Scheduler{
 		if (Math.abs(new Date().getTime()- joblist.get(0).getTime())<60*1000){
 			//RF send command
 			//remove this job
+			
+			socket.sendCmd()
 		}
 		return true;
 	}
 }
 class Job{
 	Date date;
-	int deviceID;
+	String deviceID;
 	boolean cmd;
-	Job(Date d,int id, boolean cmd){
+	Job(Date d,String id, boolean cmd){
 		this.date = d;
 		this.deviceID = id;
 		this.cmd = cmd;
