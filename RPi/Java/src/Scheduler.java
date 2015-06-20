12472c28d137cdc306b577a90e7ec7fce8d1ad36
a123zyx98 <a123zyx98@gmail.com>
2015-06-20 15:15:53 +0800

    Scheduler

---
+++ RPi/Java/src/Scheduler.java
@@ -0,0 +1,53 @@
+import java.util.Date;
+import java.util.ArrayList; 
+public class Scheduler{
+	
+	//http://www.ewdna.com/2011/12/java-timer.html
+	private ArrayList<Job> joblist;
+	//private RF rf;
+	
+	public Scheduler(){
+		joblist = new ArrayList<>();
+	}
+	public void initial(){
+		
+	}
+	boolean addJob(Job j){
+		int i;
+		if(new Date().after(j.date))
+			return false;
+		if(joblist.size()==0)
+			joblist.add(j);
+		for(i=0;i<joblist.size();i++){
+			tmp = joblist.get(i);
+			if(tmp.date.after(j.date)){
+				joblist.add(i,j);
+				break;
+			}
+		}
+		if(i==joblist.size())
+			joblist.add(j);
+		return true;
+	}
+	boolean deleteJob(Job j){
+		joblist.remove(j);
+		return true;
+	}
+	boolean checkList(){
+		if (new Date().getTime()- joblist.get(0).getTime()<60*1000){
+			//RF send command
+			//remove this job
+		}
+		return true;
+	}
+}
+class Job{
+	Date date;
+	int deviceID;
+	boolean cmd;
+	Job(Date d,int id, boolean cmd){
+		this.date = d;
+		this.deviceID = id;
+		this.cmd = cmd;
+	}
+}
