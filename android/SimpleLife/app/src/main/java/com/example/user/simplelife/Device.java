d8aaf9e41d20c6ba48c2498d4799bc5bd982a5b4
cross6180 <r03522617@ntu.edu.tw>
2015-06-23 00:02:59 +0800

    serializer test

--- android/SimpleLife/app/src/main/java/com/example/user/simplelife/Device.java
+++ android/SimpleLife/app/src/main/java/com/example/user/simplelife/Device.java
@@ -1,14 +1,23 @@
 package com.example.user.simplelife;
 
+import java.io.Serializable;
+
 /**
  * Created by bostenkg5 on 2015/6/22.
  */
-public abstract class Device {
+public class Device implements Serializable {
     protected String name;
+
+    public Device() {
+
+    }
+
     public String getName() {
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
+
+
 }
