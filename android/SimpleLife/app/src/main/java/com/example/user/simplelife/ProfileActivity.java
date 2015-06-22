84fc1d95296e2d0c84b1d5b423e7548a117ec54d
cross6180 <r03522617@ntu.edu.tw>
2015-06-22 15:32:45 +0800

    bug fix

--- android/SimpleLife/app/src/main/java/com/example/user/simplelife/ProfileActivity.java
+++ android/SimpleLife/app/src/main/java/com/example/user/simplelife/ProfileActivity.java
@@ -9,12 +9,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
 import android.widget.TextView;
-=======
 import android.widget.ImageButton;
->>>>>>> origin/master
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -29,11 +25,9 @@ public class ProfileActivity extends ActionBarActivity {
     EditText etexCPassword;
     EditText etexCurrPassword;
     Button btnSaveNewPassword;
-<<<<<<< HEAD
     TextView user_profile;
-=======
     ImageButton backButton;
->>>>>>> origin/master
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
