0fc7d28f6d3b8b3b26c983761f898ad174e3a6d2
justin81630 <justin81630@gmail.com>
2015-06-22 21:07:10 +0800

    確認Socket連線

--- RPi/Java/src/SmartHome.java
+++ RPi/Java/src/SmartHome.java
@@ -1,8 +1,9 @@
 import java.util.*;
 public class SmartHome{
-	public static RPiSocket rPiSocket = new RPiSocket();
+	public static RPiSocket rPiSocket = new RPiSocket("CrossHomeMCID");
 	public static DeviceController deviceController = new DeviceController();
 	public static DeviceInfo deviceInfo = new DeviceInfo(1000);
+	public static String MainControllerId;
 	//public static DecisionMaker decisionMaker = new DecisionMaker();
 	//public static Scheduler scheduler = new Scheduler();
 	//public static RF rf = new RF();
@@ -10,6 +11,9 @@ public class SmartHome{
 	
 	public static void main(String argv[]){
 		rPiSocket.start();
+		System.out.println("finish building\n");
+		rPiSocket.sendCmd("YA!");
+
 	}	
 
 }
