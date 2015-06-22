2468f461687e665854c6cad40e024af09c123dde
a123zyx98 <a123zyx98@gmail.com>
2015-06-22 15:25:15 +0800

    Decision Maker

--- RPi/Java/src/DecisionMaker.java
+++ RPi/Java/src/DecisionMaker.java
@@ -1,14 +1,27 @@
 public class DecisionMaker{
-	DeviceInfo device
+	DeviceInfo deviceInfo;
 	ArrayList<MotionEvent> eventlist;
+	private RF rf;
+	
 	DecisionMaker(){
 	
 	}
 	boolean Notify(String arduinoid){
 		MotionEvent event = new MotionEvent(new Date(),arduinoid);
+		for(int i=0;i<eventlist.size();i++){
+			MotionEvent tmp = eventlist.get(i);
+			if(tmp.arduinoid.equals(event.arduinoid)){
+				//check setting parameter
+				for(int j=0;j<deviceInfo.deviceCnt;j++){
+					
+				}
+				break;
+			}
+		}
+	}
+	boolean NotifyTemerature(String arduinoid){
 		
 	}
-	
 }
 
 class MotionEvent{
