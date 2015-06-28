dc3a360df52ac95d9b220a2780ef6ac5bf01ae6b
YuHuaiChen <tp6m4cj96@gmail.com>
2015-06-29 03:38:44 +0800

    garbage....force commit by github...

---
+++ RPi/C++/src/socketServer.h
@@ -0,0 +1,30 @@
+#ifndef SOCKET_SERVER_H
+#define SOCKET_SERVER_H
+
+//#include <stdio.h>
+#include <cstring>      // need for memset
+#include <sys/socket.h>
+
+class SocketServer
+{
+public:
+
+
+private:
+  SOCKET
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // SOCKET_SERVER_H
